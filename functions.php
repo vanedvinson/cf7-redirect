a4c04bb68c0795c46ca2c3ba16ace6713859f8e0
Edvin Kovacevic <ekovacevic.10@gmail.com>
2021-06-26 18:15:11 +0200

    first commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<!-- [dynamichidden dynamichidden-601 id:cfkontaktform "CF7_URL"] -->
+
+add_action('wp_head','woocommerce_js');
+
+function woocommerce_js(){
+	?>
+	<script type="text/javascript">
+		document.addEventListener( 'wpcf7submit', function( event ) {
+    		     
+            var cfkontaktform =  document.getElementById("cfkontaktform").value;
+
+            if (cfkontaktform == "https://nejchojc-personaltrainer.ch:443/ziele/schmerzfrei/") {
+                location = 'https://nejchojc-personaltrainer.ch/schmerzanalyst/vielen-dank/';
+            } else{
+                location = 'https://nejchojc-personaltrainer.ch/schmerzanalyst-start/vielen-dank/';
+            }
+        }, false );
+    </script>
+    <?php
+}
